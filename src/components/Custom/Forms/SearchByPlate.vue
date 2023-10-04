1f8ca03ef1266fad5d6a6d1899ef1852e7bb5ac7
webguru0110 <133039683+webguru0110@users.noreply.github.com>
2023-10-04 22:26:58 +0200

    Fix code format

--- src/components/Custom/Forms/SearchByPlate.vue
+++ src/components/Custom/Forms/SearchByPlate.vue
@@ -1,64 +1,65 @@
 <script setup lang="ts">
-import type { Ref } from "vue";
-import { ref } from "vue";
-import LabeledSwitch from "../../Custom/Switches/LabeledSwitch.vue";
-import CarInfo from "../Results/CarInfo.vue";
-import usePlates from "@/composables/plates";
-import useCars from "@/composables/cars";
-import useCarStore from "@/store/car";
+import type { Ref } from 'vue'
+import { ref } from 'vue'
+import LabeledSwitch from '../../Custom/Switches/LabeledSwitch.vue'
+import CarInfo from '../Results/CarInfo.vue'
+import usePlates from '@/composables/plates'
+import useCars from '@/composables/cars'
+import useCarStore from '@/store/car'
 
-const router = useRouter();
+const router = useRouter()
 
-const { searchData, loading, getByPlates, noDataFound } = usePlates();
-const { getCarInfo, carData } = useCars();
-const carStore = useCarStore();
+const { searchData, loading, getByPlates, noDataFound } = usePlates()
+const { getCarInfo, carData } = useCars()
+const carStore = useCarStore()
 
-const plate: Ref<string> = ref("");
-const serie: Ref<string> = ref("");
-const plateErr: Ref<string> = ref("");
-const serieErr: Ref<string> = ref("");
-const plateType: Ref<string> = ref("TU");
+const plate: Ref<string> = ref('')
+const serie: Ref<string> = ref('')
+const plateErr: Ref<string> = ref('')
+const serieErr: Ref<string> = ref('')
+const plateType: Ref<string> = ref('TU')
 
 const setVehiclePlateType = (value: string) => {
-  plateType.value = value;
-};
+  plateType.value = value
+}
 
 const handleSearchByPlate = async () => {
-  if (plateType.value === "TU") {
-    if (serie.value === "") {
-      serieErr.value = "Serie code is required";
+  if (plateType.value === 'TU') {
+    if (serie.value === '') {
+      serieErr.value = 'Serie code is required'
 
-      return;
+      return
     }
-    if (plate.value === "") {
-      plateErr.value = "Plate number is required";
+    if (plate.value === '') {
+      plateErr.value = 'Plate number is required'
 
-      return;
+      return
     }
-  } else {
-    if (plate.value === "") {
-      plateErr.value = "Plate number is required";
+  }
+  else {
+    if (plate.value === '') {
+      plateErr.value = 'Plate number is required'
 
-      return;
+      return
     }
   }
 
-  await getByPlates({ plate: plate.value, serie: serie.value });
-};
+  await getByPlates({ plate: plate.value, serie: serie.value })
+}
 
 watch(searchData, async () => {
   await getCarInfo({
     car: searchData.value.data.carId,
     selectedType: carStore.carType,
-  });
-});
+  })
+})
 
 const handleRedirect = () => {
   router.push({
-    name: "Categories",
+    name: 'Categories',
     params: { id: searchData.value.carId, targetType: carStore.carType },
-  });
-};
+  })
+}
 </script>
 
 <template>
@@ -71,7 +72,9 @@ const handleRedirect = () => {
       />
     </div>
     <div class="w-full flex flex-col md:flex-row flex-wrap gap-4 md:gap-0">
-      <div class="w-full md:w-6/6">Search by Plate</div>
+      <div class="w-full md:w-6/6">
+        Search by Plate
+      </div>
       <div class="w-full md:w-1/6">
         <div class="flex flex-wrap">
           <ElInput
@@ -80,43 +83,68 @@ const handleRedirect = () => {
             class="w-full"
           />
         </div>
-        <p v-if="serieErr" class="text-red-500 text-sm">
+        <p
+          v-if="serieErr"
+          class="text-red-500 text-sm"
+        >
           {{ serieErr }}
         </p>
       </div>
       <div class="w-full md:w-20">
         <div class="flex flex-wrap">
-          <span
-            class="inline-flex items-center rounded-md bg-gray-50 px-5 py-3 text-xs font-medium text-gray-600 ring-1 ring-inset ring-gray-500/10"
-          >
-            {{ plateType }}</span
-          >
+          <span class="inline-flex items-center rounded-md bg-gray-50 px-5 py-3 text-xs font-medium text-gray-600 ring-1 ring-inset ring-gray-500/10">
+            {{ plateType }}</span>
         </div>
       </div>
 
-      <div v-if="plateType === 'TU'" class="w-full md:w-1/6">
+      <div
+        v-if="plateType === 'TU'"
+        class="w-full md:w-1/6"
+      >
         <div class="flex flex-wrap">
-          <ElInput v-model="plate" placeholder="Number : 2999" class="w-full" />
+          <ElInput
+            v-model="plate"
+            placeholder="Number : 2999"
+            class="w-full"
+          />
         </div>
-        <p v-if="plateErr" class="text-red-500 text-sm">
+        <p
+          v-if="plateErr"
+          class="text-red-500 text-sm"
+        >
           {{ plateErr }}
         </p>
       </div>
     </div>
     <div>
-      <ElButton class="btn" icon="search" @click="handleSearchByPlate">
+      <ElButton
+        class="btn"
+        icon="search"
+        @click="handleSearchByPlate"
+      >
         Search
       </ElButton>
     </div>
-    <div v-if="loading" class="flex justify-center">
-      <VProgressCircular :size="35" :width="6" color="primary" indeterminate />
+    <div
+      v-if="loading"
+      class="flex justify-center"
+    >
+      <VProgressCircular
+        :size="35"
+        :width="6"
+        color="primary"
+        indeterminate
+      />
     </div>
     <div v-if="noDataFound">
       <p>NO DATA FOUND</p>
     </div>
     <div v-if="carData">
       <CarInfo :car-details="carData" />
-      <div v-if="$route.path === '/home'" class="pt-5">
+      <div
+        v-if="$route.path === '/home'"
+        class="pt-5"
+      >
         <VBtn
           color="#2d4aae"
           append-icon="mdi-arrow-right"
