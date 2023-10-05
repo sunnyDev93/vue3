914dfafe8e8241f3da15fddeda370eb6eda74a2a
sunnyDev93 <asahi.hakuru.pro@gmail.com>
2023-10-05 17:40:57 +0200

    Fix: 10

--- src/pages/my-search/categories.vue
+++ src/pages/my-search/categories.vue
@@ -1,14 +1,14 @@
 <script setup lang="ts">
-import { defineProps } from 'vue'
-import Categories from '@/views/home/components/Categories.vue'
+import { defineProps } from "vue";
+import Categories from "@/views/home/components/Categories.vue";
 
-const props = defineProps(['id'])
+const props = defineProps(["id"]);
 </script>
 
 <template>
   <div class="bg-[#f1f1fc] space-y-4">
     <div class="w-[100%] m-auto">
-      <Categories :car-id="props.id" />
+      <Categories :carId="props.id" />
     </div>
   </div>
 </template>
--- src/views/home/components/Categories.vue
+++ src/views/home/components/Categories.vue
@@ -1,101 +1,125 @@
 <script setup lang="ts">
-import { useRoute, useRouter } from 'vue-router'
-import { uniqBy } from 'lodash'
-import type { ICategory } from '../../../globalTypes'
-import useCarStore from '@/store/car'
-import PartCard from '@/components/Custom/cards/PartCard.vue'
-import { S3_STORAGE_IMAGE } from '@/composables/constant'
+import { useRoute, useRouter } from "vue-router";
+import { uniqBy } from "lodash";
+import type { ICategory } from "../../../globalTypes";
+import useCarStore from "@/store/car";
+import PartCard from "@/components/Custom/cards/PartCard.vue";
+import { S3_STORAGE_IMAGE } from "@/composables/constant";
 
-const props = defineProps(['carId'])
-const router = useRouter()
-const route = useRoute()
+const props = defineProps(["carId"]);
+const router = useRouter();
+const route = useRoute();
 
-const carStore = useCarStore()
-const loading: Ref<boolean> = ref(false)
-const carCategoriesData: Ref<any> = ref(null)
+const carStore = useCarStore();
+const loading: Ref<boolean> = ref(false);
+const carCategoriesData: Ref<any> = ref(null);
 
 const handleRedirect = (treeID: string, treeName: string) => {
-  router.push({ name: 'Product List', params: { categoryId: treeID, groupName: treeName, carId: props.carId } })
-}
+  router.push({
+    name: "Product List",
+    params: { categoryId: treeID, groupName: treeName, carId: props.carId },
+  });
+};
 
 const handlePrimaryCategoryClick = (values: ICategory[]) => {
-  carStore.setFilterTitles(values[0].ROOT_NODE_TEXT)
-  carStore.setFilteredCategories(uniqBy(values, 'NODE_1_TEXT'))
-  carStore.setAllCategories(values)
-  carStore.setFilterLevel(2)
-  router.push({ name: 'Categories', params: { id: props.carId, treeName: values[0].ROOT_NODE_TEXT } })
-}
+  carStore.setFilterTitles(values[0].ROOT_NODE_TEXT);
+  carStore.setFilteredCategories(uniqBy(values, "NODE_1_TEXT"));
+  carStore.setAllCategories(values);
+  carStore.setFilterLevel(2);
+  router.push({
+    name: "Categories",
+    params: { id: props.carId, treeName: values[0].ROOT_NODE_TEXT },
+  });
+};
 
 const handleSecondLevelClick = (item: ICategory) => {
   if (item.STR_LEVEL === 2) {
-    handleRedirect(item.NODE_1_STR_ID, item.NODE_1_TEXT)
-  }
-  else {
-    carStore.setFilterTitles(item.NODE_1_TEXT)
+    handleRedirect(item.NODE_1_STR_ID, item.NODE_1_TEXT);
+  } else {
+    carStore.setFilterTitles(item.NODE_1_TEXT);
 
-    const categories = carStore.allCategories.filter((category: ICategory) => category.NODE_1_STR_ID === item.NODE_1_STR_ID)
+    const categories = carStore.allCategories.filter(
+      (category: ICategory) => category.NODE_1_STR_ID === item.NODE_1_STR_ID
+    );
 
-    carStore.setFilteredCategories(uniqBy(categories, 'NODE_2_TEXT'))
-    carStore.setFilterLevel(3)
-    router.push({ name: 'Categories', params: { id: props.carId, treeName: item.ROOT_NODE_TEXT, strid: item.NODE_1_STR_ID, strName: item.NODE_1_TEXT } })
+    carStore.setFilteredCategories(uniqBy(categories, "NODE_2_TEXT"));
+    carStore.setFilterLevel(3);
+    router.push({
+      name: "Categories",
+      params: {
+        id: props.carId,
+        treeName: item.ROOT_NODE_TEXT,
+        strid: item.NODE_1_STR_ID,
+        strName: item.NODE_1_TEXT,
+      },
+    });
   }
-}
+};
 
 const handleThirdLevelClick = (item: ICategory) => {
   if (item.STR_LEVEL === 3) {
-    item.NODE_2_STR_ID && item.NODE_2_TEXT && handleRedirect(item.NODE_2_STR_ID, item.NODE_2_TEXT)
-  }
-  else {
-    item.NODE_2_TEXT && carStore.setFilterTitles(item.NODE_2_TEXT)
+    item.NODE_2_STR_ID &&
+      item.NODE_2_TEXT &&
+      handleRedirect(item.NODE_2_STR_ID, item.NODE_2_TEXT);
+  } else {
+    item.NODE_2_TEXT && carStore.setFilterTitles(item.NODE_2_TEXT);
 
-    const categories = carStore.allCategories.filter((category: ICategory) => category.NODE_2_STR_ID === item.NODE_2_STR_ID)
+    const categories = carStore.allCategories.filter(
+      (category: ICategory) => category.NODE_2_STR_ID === item.NODE_2_STR_ID
+    );
 
-    carStore.setFilteredCategories(uniqBy(categories, 'NODE_3_TEXT'))
-    carStore.setFilterLevel(4)
-    router.push({ name: 'Categories', params: { id: props.carId, treeName: item.ROOT_NODE_TEXT, strid: item.NODE_2_STR_ID, strName: item.NODE_2_TEXT } })
+    carStore.setFilteredCategories(uniqBy(categories, "NODE_3_TEXT"));
+    carStore.setFilterLevel(4);
+    router.push({
+      name: "Categories",
+      params: {
+        id: props.carId,
+        treeName: item.ROOT_NODE_TEXT,
+        strid: item.NODE_2_STR_ID,
+        strName: item.NODE_2_TEXT,
+      },
+    });
   }
-}
+};
 
 const handleFourthLevlClick = (item: ICategory) => {
-  item.NODE_3_STR_ID && item.NODE_3_TEXT && handleRedirect(item.NODE_3_STR_ID, item.NODE_3_TEXT)
-}
+  item.NODE_3_STR_ID &&
+    item.NODE_3_TEXT &&
+    handleRedirect(item.NODE_3_STR_ID, item.NODE_3_TEXT);
+};
 
 onMounted(() => {
-  carCategoriesData.value = carStore.carCategories
-  console.log(carStore.filterLevel)
-})
+  carCategoriesData.value = carStore.carCategories;
+});
 
 watchEffect(() => {
-  carCategoriesData.value = carStore.carCategories
-})
+  carCategoriesData.value = carStore.carCategories;
+});
 
-watch(() => route.name, () => {
-  if (route.name !== 'Categories' && route.name !== 'Product List')
-    carStore.setFilterLevel(1)
-})
-
-window.addEventListener('popstate', () => {
-  // eslint-disable-next-line curly
-  if (carStore.filterLevel > 1 && route.name === 'Categories') {
-    carStore.filterLevel--
+watch(
+  () => route.name,
+  () => {
+    if (route.name !== "Categories" && route.name !== "Product List")
+      carStore.setFilterLevel(1);
   }
-  carStore.removeFilterTitle()
-})
+);
+
+window.addEventListener("popstate", () => {
+  // eslint-disable-next-line curly
+  if (carStore.filterLevel > 1 && route.name === "Categories") {
+    carStore.filterLevel--;
+  }
+  carStore.removeFilterTitle();
+});
 </script>
 
 <template>
-  <div
-    v-loading.fullscreen.lock="loading"
-    class="p-3 space-y-4 scroll-smooth"
-  >
+  <div v-loading.fullscreen.lock="loading" class="p-3 space-y-4 scroll-smooth">
     <div class="w-full flex gap-4">
       <div v-if="carStore.filterLevel === 1">
         <div v-if="carCategoriesData">
           <div class="flex flex-wrap flex-col md:flex-row gap-6 w-full">
-            <div
-              v-for="(values, key) in carCategoriesData"
-              :key="key"
-            >
+            <div v-for="(values, key) in carCategoriesData" :key="key">
               <PartCard
                 :name="key"
                 :image="`${S3_STORAGE_IMAGE}img.sections/100x100/${values[0].ROOT_NODE_STR_ID}.png`"
@@ -108,7 +132,7 @@ window.addEventListener('popstate', () => {
       <div v-if="carStore.filterLevel === 2">
         <div class="flex flex-wrap flex-col md:flex-row gap-6 w-full">
           <div
-            v-for="item in carStore.filteredCategories "
+            v-for="item in carStore.filteredCategories"
             :key="item.NODE_1_STR_ID"
           >
             <PartCard
@@ -122,7 +146,7 @@ window.addEventListener('popstate', () => {
       <div v-if="carStore.filterLevel === 3">
         <div class="flex flex-wrap flex-col md:flex-row gap-6 w-full">
           <div
-            v-for="item in carStore.filteredCategories "
+            v-for="item in carStore.filteredCategories"
             :key="item.NODE_2_STR_ID ?? item.NODE_1_STR_ID"
           >
             <PartCard
@@ -136,7 +160,7 @@ window.addEventListener('popstate', () => {
       <div v-if="carStore.filterLevel === 4">
         <div class="flex flex-wrap flex-col md:flex-row gap-6 w-full">
           <div
-            v-for="item in carStore.filteredCategories "
+            v-for="item in carStore.filteredCategories"
             :key="item.NODE_3_STR_ID ?? item.NODE_1_STR_ID"
           >
             <PartCard
